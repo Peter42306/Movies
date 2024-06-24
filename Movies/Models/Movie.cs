708da3b057b9bbd6db8e99159835d0a9c4aaa268
Petr Zalizko <pzalizko@gmail.com>
2024-06-24 12:45:40 +0300

    Add project files.

---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace Movies.Models
+{
+    public class Movie
+    {
+        // Id фильма в БД
+        public int Id { get; set; }
+
+        // Название фильма
+        public string? Title { get; set; }
+
+        // Режиссер фильма
+        public string? Director { get; set; }
+
+        // Жанр фильма
+        public string? Genre { get; set; }
+
+        // Год выпуска фильма
+        public int ReleaseYear { get; set; }
+
+        // Путь к постеру фильма
+        public string? PosterPath { get; set; }
+
+        // Описание фильма
+        public string? Description { get; set; }
+    }
+}
